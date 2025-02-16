69eefeee613219e4c1a6736063eab6f2cc4377f7
Stanislav M <phoenix0597@yandex.ru>
2025-02-16 14:42:18 +0300

    Add app config

---
+++ src/config/config.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str = ""
+    SECRET_KEY: str = ""
+    WEBHOOK_SECRET_KEY: str = ""
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    model_config = SettingsConfigDict(
+        env_file=[BASE_DIR / ".env.sample", BASE_DIR / ".env"],
+        env_file_encoding="utf-8",
+    )
+
+
+settings = Settings()
