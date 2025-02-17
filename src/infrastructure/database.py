ffd0d8d276bbbb81e5d5878d15fb63b38db17817
Stanislav M <phoenix0597@yandex.ru>
2025-02-17 11:12:48 +0300

    Add database.py

---
+++ src/infrastructure/database.py
@@ -0,0 +1,16 @@
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+from src.config.config import settings
+
+engine = create_async_engine(settings.DATABASE_URL, echo=True)
+async_session = async_sessionmaker(engine, expire_on_commit=False)
+Base = DeclarativeBase()
+
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
