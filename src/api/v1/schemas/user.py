f2ec3397513aaccbbe272a44c616cb334c7ff150
Stanislav M <phoenix0597@yandex.ru>
2025-02-17 12:00:13 +0300

    Add user's pydantic-scheme

---
+++ src/api/v1/schemas/user.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, EmailStr, ConfigDict
+from typing import List, Optional
+
+
+class UserBase(BaseModel):
+    email: EmailStr
+    full_name: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserUpdate(UserBase):
+    password: Optional[str] = None
+
+
+class UserInDB(UserBase):
+    id: int
+    is_admin: bool
+
+    model_config = ConfigDict(from_attributes=True)
