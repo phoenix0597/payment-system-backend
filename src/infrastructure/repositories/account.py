d437c1626320f257d7ac31ce97663b88e487d9ad
Stanislav M <phoenix0597@yandex.ru>
2025-02-17 15:45:07 +0300

    Edit account's repository layer

--- src/infrastructure/repositories/account.py
+++ src/infrastructure/repositories/account.py
@@ -1,5 +1,4 @@
 from decimal import Decimal
-from typing import List
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -11,10 +10,10 @@ class AccountRepository(BaseRepository[Account]):
     def __init__(self, session: AsyncSession):
         super().__init__(Account, session)
 
-    async def get_by_user_id(self, user_id: int) -> List[Account]:
+    async def get_by_user_id(self, user_id: int):
         query = select(Account).where(Account.user_id == user_id)  # type: ignore
         result = await self.session.execute(query)
-        return list(result.scalars().all())
+        return result.scalars().all()
 
     async def update_balance(self, account_id: int, amount: Decimal) -> Account:
         account = await self.get(account_id)
