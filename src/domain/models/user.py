6b612bcea5fc6727bbe30348b6fe0df6b74cbb71
Stanislav M <phoenix0597@yandex.ru>
2025-02-17 11:42:40 +0300

    Add user's SQLAlchemy-model

---
+++ src/domain/models/user.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+from src.infrastructure.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    full_name = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_admin = Column(Boolean, default=False)
+
+    accounts = relationship(
+        "Account", back_populates="user", cascade="all, delete-orphan"
+    )
+    payments = relationship(
+        "Payment", back_populates="user", cascade="all, delete-orphan"
+    )
