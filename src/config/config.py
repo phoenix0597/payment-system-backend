8f9440d88638ae7c30071499cccaa7a80604873d
Stanislav M <phoenix0597@yandex.ru>
2025-02-20 23:56:03 +0300

    Add API_PREFIX to config

--- src/config/config.py
+++ src/config/config.py
@@ -9,7 +9,8 @@ class Settings(BaseSettings):
     SECRET_KEY: str = ""
     WEBHOOK_SECRET_KEY: str = ""
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    TOKEN_URL: str = ""
+    API_PREFIX: str = "/api/v1"
+    TOKEN_URL: str = "/auth/token"
 
     model_config = SettingsConfigDict(
         env_file=[BASE_DIR / ".env.sample", BASE_DIR / ".env"],
