cd211243582b725400c18ba808bd246a37ab1359
phoenix <phoenix0597@yandex.ru>
2025-02-25 12:26:23 +0300

    Fix get_by_transaction_id func in repository layer

--- src/infrastructure/repositories/payment.py
+++ src/infrastructure/repositories/payment.py
@@ -9,7 +9,9 @@ class PaymentRepository(BaseRepository[Payment]):
         super().__init__(Payment, session)
 
     async def get_by_transaction_id(self, transaction_id: str):
-        return await self.get(transaction_id)
+        query = select(self.model).where(self.model.transaction_id == transaction_id)
+        result = await self.session.execute(query)
+        return result.scalars().first()
 
     async def get_by_user_id(self, user_id: int):
         return await self.get_by_filter(Payment.user_id == user_id)
