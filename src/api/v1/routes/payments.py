7f1272f22b8eeb63748cc83bc22eadbd2707326b
phoenix <phoenix0597@yandex.ru>
2025-02-26 14:43:22 +0300

    Add logging to payments route module (get user's payment)

--- src/api/v1/routes/payments.py
+++ src/api/v1/routes/payments.py
@@ -29,4 +29,5 @@ async def get_user_payments(
     current_user=Depends(get_current_user),
     payment_service: PaymentService = Depends(get_payment_service),
 ):
+    log.info(f"Fetching payments for user_id: {current_user.id}")
     return await payment_service.get_payments_by_user_id(current_user.id)
